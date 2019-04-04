514ec01f263ec6116e39fd6bc2bb1bf113eb31fe
YuhanLiin <linyuhan0315@hotmail.com>
2019-04-03 23:30:09 -0400

    Refactored components to not mutate props

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-function Question(text, choices) {
+function Question(text, key, choices) {
     return {
         text: text,
         choices: choices,
-    }
-}
-
-function Answer(key) {
-    return {
-        key: key,
-        value: null,
+        key: key
     }
 }
 
@@ -19,6 +13,15 @@ function Choice(text, value) {
     }
 }
 
+function Scores(risk, age, duration, experience) {
+    return {
+        risk: risk,
+        age: age,
+        duration: duration,
+        experience: experience,
+    }
+}
+
 Vue.component('survey', {
     template: '#survey-template',
 
@@ -26,58 +29,63 @@ Vue.component('survey', {
         return {
             title: 'Investment Option Evaluator',
             questions: [
-                Question('What risk level are you comfortable with?',
+                Question('What risk level are you comfortable with?', 'risk',
                     [
                         Choice('Nearly no risk', 1),
                         Choice('Low risk and possible loss of money', 2),
                         Choice('Moderate risk', 3),
                         Choice('High risk', 4),
                     ]),
-                Question('How old are you?',
+                Question('How old are you?', 'age',
                     [
                         Choice('Under 30', 1),
                         Choice('30 to 60', 2),
                         Choice('Over 60', 3),
                     ]),
-                Question('How long do you want to keep your money invested?',
+                Question('How long do you want to keep your money invested?', 'duration',
                     [
                         Choice('1 to 3 years', 1),
                         Choice('3 to 5 years', 2),
                         Choice('Over 5 years', 3),
                     ]),
-                Question('How much experience do you have with investing?',
+                Question('How much experience do you have with investing?', 'experience',
                     [
                         Choice('No experience', 1),
                         Choice("I've dabbled a bit", 2),
                         Choice("I know what I'm doing", 3),
                     ]),
             ],
-
-            answers: [
-                Answer('risk'),
-                Answer('age'),
-                Answer('duration'),
-                Answer('experience'),
-            ],
+            scores: Scores(null, null, null, null),
         }
     },
 
     methods: {
         submit: function() {
-            var answers = this.answers.map(function (answer) { return answer.value });
-            console.log(answers);
+            console.log(JSON.stringify(this.scores, null, 2))
         }
     }
 })
 
 Vue.component('question', {
     template: '#question-template',
-    props: ['question', 'answer_val'],
+    props: ['question', 'answer'],
+
+    methods: {
+        updateAnswer: function(answer) {
+            this.$emit('update:answer', answer);
+        }
+    }
 })
 
 Vue.component('choice', {
     template: '#choice-template',
-    props: ['choice', 'answer_val'],
+    props: ['choice', 'answer'],
+
+    methods: {
+        updateAnswer: function() {
+            this.$emit('update:answer', this.choice.value);
+        }
+    }
 })
 
 var app = new Vue({
